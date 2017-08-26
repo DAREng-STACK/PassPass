9cdcf12c6855b5f5f88920c65783a2ed4b5ecd60
Ryan <Ryan@Toms-MacBook.local>
2017-08-25 18:16:42 -0700

    adds delete button on YourProfile

--- react-client/src/components/loggedInComponents/YourProfile.jsx
+++ react-client/src/components/loggedInComponents/YourProfile.jsx
@@ -9,15 +9,15 @@ class YourProfile extends React.Component {
       userId: this.props.profileData.id,
       havePendingPasses: false,
       pendingPasses: [],
-      havePurchasedPasses: false,
-      purchasedPasses: []
+      haveCurrentlyAvailablePasses: false,
+      currentlyAvailablePasses: [],
+      haveExpiredPasses: false,
+      expiredPasses: []
     };
   }
 
   componentWillMount () {
-    this.getPendingPasses();
-    this.getAvailablePasses();
-    this.getExpiredPasses();
+    this.getAllPasses();
   }
 
   deletePendingPass(pass) {
@@ -36,32 +36,16 @@ class YourProfile extends React.Component {
     console.log(pass)
   }
 
-  getUserPurchasedPasses() {
-   $.ajax({
-      method: 'POST',
-      url: '/passes/purchased',
-      contentType: 'application/json',
-      data: JSON.stringify({userId: this.state.userId}),
-      success: function(purchasedPasses) {
-				if (purchasedPasses.length === 0) {
-        } else {
-          this.setState({havePurchasedPasses: true, purchasedPasses: purchasedPasses})
-        }
-      }.bind(this),
-      error: function(xhr, error) {
-        console.log('error:', error);
-      }
-    });
-  }
 
-  getPendingPasses() {
-    var context = this;
+
+  getAllPasses() {
     $.ajax({
       method: 'POST',
-      url: '/passes/pending',
+      url: '/passes/all',
       contentType: 'application/json',
       data: JSON.stringify({userId: this.state.userId}),
       success: function(pendingPasses) {
+        console.log(pendingPasses, "PENDINGDPASSES")
 				if (pendingPasses.length === 0) {
         } else {
           this.setState({havePendingPasses: true, pendingPasses: pendingPasses})
@@ -97,44 +81,7 @@ class YourProfile extends React.Component {
     });
   }
 
-  getAvailablePasses() {
-    $.ajax({
-      method: 'POST',
-      url: '/passes/available',
-      contentType: 'application/json',
-      data: JSON.stringify({userId: this.state.userId}),
-      success: function(availablePasses) {
-        if (availablePasses.length === 0) {
-          console.log(availablePasses, '@@@@@@@ NO AVAILABLE');
-        } else {
-          this.setState({haveAvailablePasses: true})
-        }
-      }.bind(this),
-      error: function(err) {
-        console.log(err, '###### ERROR')
-      }
-    });
-  }
 
-  getExpiredPasses() {
-    $.ajax({
-      method: 'POST',
-      url: '/passes/expired',
-      contentType: 'application/json',
-      data: JSON.stringify({userId: this.state.userId}),
-      success: function(expiredPasses) {
-        if (expiredPasses.length === 0) {
-          console.log(expiredPasses, '@@@@@@@ NO EXPIRED');
-        } else {
-          console.log(expiredPasses, '@@@@@@@ YES EXPIRED');
-          this.setState({haveExpiredPasses: true})
-        }
-      }.bind(this),
-      error: function(err) {
-        console.log(err, '####### ERROR');
-      }
-    });
-  }
 
   render() {
     return (
@@ -192,9 +139,6 @@ class YourProfile extends React.Component {
               </ul>
           }
         </ul>
-        <div className="profileQuote">
-          Workout Quote
-        </div>
       </div>
     )
   }
