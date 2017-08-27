c451d421a2ac5a221e9a7aa5b95aa28dce856265
Ryan <Ryan@Toms-MacBook.local>
2017-08-26 17:12:38 -0700

    dynamically renders restricted gyms in pending passes

--- react-client/src/components/loggedInComponents/PendingPasses.jsx
+++ react-client/src/components/loggedInComponents/PendingPasses.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Checkout from './Checkout.jsx';
+import RestrictedGyms from './RestrictedGyms.jsx';
 
 var PendingPasses = (props) => (
 
@@ -12,9 +13,18 @@ var PendingPasses = (props) => (
         <li>
           <strong>Pass End:</strong> Date: {new Date(props.pass.period_end.slice(0, 10)).toDateString().slice(4)}
         </li>
+        {props.pass.restrictedStudios.length > 0 &&
+         <li>
+           {props.pass.restrictedStudios.map((gym, index) =>
+             <RestrictedGyms gym={gym} key={index}/>
+           )}
+         </li>
+       }
+       {props.pass.restrictedStudios.length === 0 &&
         <li>
           <strong>Restricted Gyms:</strong>
         </li>
+      }
         <li>
           <strong>Price:</strong> {props.pass.current_price}
         </li>
@@ -53,21 +63,6 @@ var PendingPasses = (props) => (
 
   </div>
 );
-
-
-
-// [ RowDataPacket {
-//     id: 5,
-//     perspective_buyer_id: 4,
-//     for_sale_block_id: 5,
-//     pass_volume: 9,
-//     seller_id: 1,
-//     current_price: 8.99,
-//     period_start: 2017-04-20T07:00:00.000Z,
-//     period_end: 2017-04-30T07:00:00.000Z,
-//     passes_sold: 5 } ]
-
-// [ RowDataPacket { first_name: 'Billy', email: 'billy@bob.com' } ]
 
 
 
